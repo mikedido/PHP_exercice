d3f5c09877a37ab25ab718de9ed8149378c8fd21
MGU <mahdigueffaz@gmail.com>
2016-12-24 19:32:29 +0100

    add

---
+++ TP_protectedByPassword/formulaire.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html >
+<head>
+	<meta charset="utf-8" />
+	<title>formulaire d'accès</title>
+</head>
+<body>
+	<?php echo 'hello world'; ?>
+	<form action="secret.php" method="post">
+		<input type="password" name="mdp"/>
+		<input type="submit" value="Valider">
+	</form>
+</body>
+</html>
---
+++ TP_protectedByPassword/secret.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+Vérifcation du mot de passe
+*/
+if (isset($_POST['mdp'])) {
+
+	if($_POST['mdp'] =="kangourou") {
+		echo 'vous avez accès au portail de la NASA';
+	} else {
+		echo 'vous avez pas accès';
+	}
+
+}
+
+
+?>
