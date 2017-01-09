6a19b8e29559fb5d817ce8b5aef0938df832e22b
MGU <mahdigueffaz@gmail.com>
2017-01-09 23:08:08 +0100

    correct bug

--- TP_miniChat/comments.php
+++ TP_miniChat/comments.php
@@ -1,19 +1,30 @@
 <?php 
 
-try {
+if(isset($_POST['login']) AND isset($_POST['comment'])) {
+    if (!empty($_POST['login']) AND !empty($_POST['comment'])) {
 
-$db = new PDO('mysql:host=localhost;dbname=mahdi_tp1;charst=utf8', 'root', 'ma1985gu'); 
+	try {
 
-}catch(Exception $e) {
+		$db = new PDO('mysql:host=localhost;dbname=mahdi_tp1;charst=utf8', 'root', 'ma1985gu'); 
 
-	die($e->getMessage());
-}
+	}catch(Exception $e) {
+		die($e->getMessage());
+	}
 
 
-$query = $db->prepare('INSERT INTO comments (login, comment) VALUES(:login, :comment)');
+	$query = $db->prepare('INSERT INTO comments (login, comment) VALUES(:login, :comment)');
 
-$query->execute(array(
+	$query->execute(array(
 		'login'   => $_POST['login'],
 		'comment' => $_POST['comment']));
 
 
+	header('location:sender.php');
+
+    }
+   	
+	header('location:sender.php');
+}
+
+
+	header('location:sender.php');
