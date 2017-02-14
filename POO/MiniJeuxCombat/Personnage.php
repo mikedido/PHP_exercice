fd0091e5f48d7129564452593729f16a0e6e0cf1
MGU <mahdigueffaz@gmail.com>
2017-02-14 23:41:57 +0100

    init TP1 POO

---
+++ POO/MiniJeuxCombat/Personnage.php
@@ -0,0 +1,18 @@
+<?php
+
+class Personnage
+{
+  private $_force;
+  private $_experience;
+  private $_degats;
+
+  public function __construct()
+  {
+    echo 'I am a test class';
+  }
+
+  public function parler()
+  {
+
+  }
+}
