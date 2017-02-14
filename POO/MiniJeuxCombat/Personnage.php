2a62b9877b2ff05377ab3a88305bcee13765f7c0
MGU <mahdigueffaz@gmail.com>
2017-02-14 23:59:16 +0100

    prepare Personnage class

--- POO/MiniJeuxCombat/Personnage.php
+++ POO/MiniJeuxCombat/Personnage.php
@@ -2,17 +2,67 @@
 
 class Personnage
 {
-  private $_force;
-  private $_experience;
+  private $_id;
+  private $_nom;
   private $_degats;
 
-  public function __construct()
+  public function __construct(array $donnees)
   {
-    echo 'I am a test class';
+      $this->hydrate($donnees);
   }
 
-  public function parler()
+
+  public function hydrate(array $donnees)
+  {
+      foreach ($donnees as $key => $value) {
+        $methode = 'set'.ucfirst($key);
+        if (method_exists($this, $methode)) {
+          $this->$methode($value);
+        }
+      }
+  }
+
+  public function faireDegats(Personnage $pers)
   {
 
   }
+
+  //setters
+  public function setId($id)
+  {
+    if (is_int($id)) {
+      $this->_id = $id;
+    }
+  }
+
+  public function setNom($nom)
+  {
+    if (is_string($nom)) {
+      $this->_nom = $nom;
+    }
+  }
+
+  public function setDegats($degat)
+  {
+    if (is_int($degat) AND $degat > 0) {
+      $this->_degats = $degats;
+    }
+  }
+
+  //getters
+  public function getId()
+  {
+    return $this->_id;
+  }
+
+  public function getNom()
+  {
+    return $this->_nom;
+  }
+
+  public function getDegats()
+  {
+    return $this->_degats;
+  }
+
 }
